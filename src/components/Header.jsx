eb160e548fddd373193a5324bd6cdd5af749877e
Leticia Santos <leticia.s.poveda@gmail.com>
2022-10-29 21:09:03 +0200

    navbar fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import headerPic from "../assets/home-blue.png"
 
 function Header() {
     return (
-      <nav className="navbar" style={{ backgroundColor: "#5fccfc" }}>
+      <nav className="navbar sticky-top" style={{ backgroundColor: "#5fccfc" }}>
         <div className="container-fluid mx-auto d-block">
           <Link to="/">
             <img src={headerPic} alt="home" />
